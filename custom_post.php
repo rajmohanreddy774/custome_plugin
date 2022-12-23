fde3c834d078fb03d5993919b0a8b9c28a42fa86
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2022-12-23 18:40:37 +0530

    feat: adding custom_post_plugin

---
+++ custom_post.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: custom post new
+ * Version: 1.0
+ * Author: Raj
+ * Author URI: http://localhost:10030/custom_post;
+ * Description: Easily add code snippets in WordPress. Insert scripts to the header and footer, add PHP code snippets with conditional logic, insert ads pixel, custom content, and more.
+ */
